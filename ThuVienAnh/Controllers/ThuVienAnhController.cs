fa39f5bfbcb54ee2db895cd7a972ce0efd66ad8d
thinh <thinhqn999@gmail.com>
2025-02-07 21:57:22 +0700

    Add project files.

---
+++ ThuVienAnh/Controllers/ThuVienAnhController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThuVienAnh.Controllers
+{
+  public class ThuVienAnhController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
