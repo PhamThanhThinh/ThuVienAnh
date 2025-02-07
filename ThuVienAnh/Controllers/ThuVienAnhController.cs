149457092a6a8a2d3d7006e699868451308889fe
thinh <thinhqn999@gmail.com>
2025-02-07 23:00:11 +0700

    final web upload ảnh

--- ThuVienAnh/Controllers/ThuVienAnhController.cs
+++ ThuVienAnh/Controllers/ThuVienAnhController.cs
@@ -4,9 +4,48 @@ namespace ThuVienAnh.Controllers
 {
   public class ThuVienAnhController : Controller
   {
+    private readonly string rootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot//img");
+
     public IActionResult Index()
     {
+      bool directoryExists = Directory.Exists(rootPath);
+
+      if (!directoryExists)
+      {
+        Directory.CreateDirectory(rootPath);
+      }
+
+      List<string> image = Directory.GetFiles(rootPath).Select(Path.GetFileName).ToList();
+      return View(image);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(IFormFile file)
+    {
+      if (file != null)
+      {
+        // lấy tên file
+        //var path = Path.Combine(rootPath, file.Name);
+        var path = Path.Combine(rootPath, Guid.NewGuid() 
+          + Path.GetExtension(file.FileName));
+
+        // Lưu file vào thư mục
+        // file directory
+        using (var fileDirectory = new FileStream(path, FileMode.Create))
+        {
+          await file.CopyToAsync(fileDirectory);
+        }
+
+        return RedirectToAction("Index");
+        //return Redirect(nameof(Index));
+
+      }
+      //else
+      //{
+
+      //}
       return View();
     }
+
   }
 }
--- ThuVienAnh/Program.cs
+++ ThuVienAnh/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=ThuVienAnh}/{action=Index}/{id?}");
 
 app.Run();
